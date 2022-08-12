0183c53ebafead9590eb2ddd1aa9f3c6b1a0c14b
yeon228 <cumber88@naver.com>
2022-08-12 21:04:32 +0900

    8/12

--- src/test/java/beandefinition/BeanDefinitionTest.java
+++ src/test/java/beandefinition/BeanDefinitionTest.java
@@ -5,21 +5,22 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-
+//    AnnotationConfigApplicationContext ac  = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
     @Test
     @DisplayName("빈 설정 메타정보 확인")
-    void findApplicationBean(){
-        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+    void findApplicationbean(){
+        String[] beandefinitionnames= ac.getBeanDefinitionNames();
+        for(String beandefinitionname: beandefinitionnames){
+            BeanDefinition beandefinition= ac.getBeanDefinition(beandefinitionname);
 
-            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
-                System.out.println("beanDefinitionName = " + beanDefinitionName + "beanDefinition" + beanDefinition);
-            }
-        }
-    }
+            if(beandefinition.getRole()== beandefinition.ROLE_APPLICATION){
+                System.out.println("beandefinitionname="+ beandefinitionname+"beandefinition"+ beandefinition);
+       }
+   }
+}
 }
 
