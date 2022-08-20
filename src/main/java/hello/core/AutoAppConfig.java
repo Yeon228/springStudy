0f6519d1bb703cebf563e16b6d285ec60a84146a
yeon228 <cumber88@naver.com>
2022-08-20 18:04:52 +0900

    annotation option

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(
+        basePackages = {"hello.core"} , // 대상 하위 패키지에서만 컴포넌트 스캔
+        basePackageClasses = AutoAppConfig.class, // 해당 클래스의 패키지의 하위 파일들 스캔
+        //Default @ComponetScan 이 붙은 패키지의 하위 파일 모두 탐색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
