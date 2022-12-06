9ac18fcf2cdad88d73ab8fc748f99c2c517fd931
Yeon228 <cumber88@naver.com>
2022-12-07 06:47:10 +0900

    learn how to use default scan option

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //스캔 기본값 -> AppConfig이 속한 패키지의 하위 파일 탐색
+        //basePackages = "hello.core", //해당 패키지 아래로 탐색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)//Configuration Annotation 이 등록된 클래스는 등록 x ex)AppConfig,TestConfig..
 )
 public class AutoAppConfig {
     //auto wired 기능 사용
-
 }
