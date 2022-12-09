2b754591f51bc7b108ee44073bde9272f6809841
Yeon228 <cumber88@naver.com>
2022-12-10 05:26:36 +0900

    make custom filter annotation

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,11 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
     //auto wired 기능 사용
+
+
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+    //자동 등록 빈과 수동 등록 빈의 이름이 같은 경우가 발생할 시 수동 등록 빈이 우선권을 가짐
 }
