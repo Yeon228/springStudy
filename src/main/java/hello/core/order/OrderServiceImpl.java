35ec1736e25b0d9302fc62bc00fc8a0feab60c76
Yeon228 <cumber88@naver.com>
2022-12-11 03:11:02 +0900

    various DI method

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,9 +12,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
+    //@Autowired private final MemberRepository memberRepository; 필드 의존관계 자동 주입
+    //외부에서 변경이 힘들어 추천 x>
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    //setter(수정자) 의존관계 주입
+    //빈 등록 이후 주입함
+    //선택적 의존관계 주입
+    //    @Autowired
+    //    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+    //        this.discountPolicy = discountPolicy;
+    //    }
+
+    //생성자 의존관계 주입
+    //불변, 필수 의존관계에 사용
+    //생성자가 1개만 있으면 @Autowired 없이도 자동 주입됨.
+    //bean 등록과 함께 의존관계 주입. new OrderServiceImpl()시점에 주입됨
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
