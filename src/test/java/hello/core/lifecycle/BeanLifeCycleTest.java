a54f45e01aef3501baf1b47763328310cd9dc237
Yeon228 <cumber88@naver.com>
2022-12-21 10:52:22 +0900

    call bean lifecycle bethod by bean annotation

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -20,7 +20,11 @@ public class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig{
 
-        @Bean
+        @Bean(initMethod = "init", destroyMethod = "close")//init,close 메서드 지정
+        //메서드 이름 자유롭게 사용 가능, 스프링 빈이 스프링 코드에 의존안함
+        //외부 라이브러리에도 초기화, 종료 메서드 지정 가능 (중요)
+        //destroyMethod는 지정을 안해줘도 기본적으로 사용되는 종료 메서드의 이름을 추론해서 호출해줌
+        //추론기능 사용 안하고 싶을 시 destroyMethod=""로 하면됨
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("asdf");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 // InitializingBean, DisposableBean (초기화, 소멸) 인터페이스는 전적으로 스프링에 의존하는 단점 있음
 // 메서드 이름 변경이 불가능함. 지금은 거의 사용 x
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -32,17 +32,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     //의존관계 주입이 끝나면 호출되는 메서드
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    //빈 삭제시 호출되는 메서드
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    //빈 삭제시 호출되는 메서드@Override
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
