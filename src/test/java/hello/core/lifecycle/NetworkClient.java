cb2392d05d76249c4ba8ed14fc1d79c8af56cc87
Yeon228 <cumber88@naver.com>
2022-12-21 10:42:26 +0900

    call bean lifecycle method by interface

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+// InitializingBean, DisposableBean (초기화, 소멸) 인터페이스는 전적으로 스프링에 의존하는 단점 있음
+// 메서드 이름 변경이 불가능함. 지금은 거의 사용 x
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public String getUrl() {
@@ -27,5 +29,20 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close : " + url);
+    }
+
+    //의존관계 주입이 끝나면 호출되는 메서드
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    //빈 삭제시 호출되는 메서드
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
